2a16a9647a32398920608b5b62eab72c5812b7ba
Sean Knowles <s@seanknowles.dev>
2022-01-08 12:28:52 +0000

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Focus from "./src/features/focus/Focus";
 
 export default function App() {
+  const [focusSubject, setFocusSubject] = useState(null);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {focusSubject ? <Text>Log me my focus subject</Text> : <Focus />}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#252250",
   },
 });
